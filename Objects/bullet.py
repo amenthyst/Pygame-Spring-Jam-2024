9fc4a97e0073134686b93bfa84343897ba6b7c73
Will Daras <willdaras@gmail.com>
2024-03-29 15:41:39 +0000

    bullet damage setup, ready for collision and enemies

--- Objects/bullet.py
+++ Objects/bullet.py
@@ -1,5 +1,6 @@
 import pygame
 import images
+import tags
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, position, velocity):
@@ -7,6 +8,12 @@ class Bullet(pygame.sprite.Sprite):
         self.image = images.renderbullet()
         self.rect = self.image.get_rect(center=position)
         self.velocity = pygame.math.Vector2(velocity)
+        self.damage = 1
     def update(self):
         self.rect.x += self.velocity.x
         self.rect.y += self.velocity.y
+
+    def on_collide(self, other):
+        if other is not tags.Damageable:
+            return
+        other.damage(self.damage)
