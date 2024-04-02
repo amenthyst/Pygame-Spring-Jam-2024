8192961bbcce6bddca5fda0042a8c2514d4dca81
Will Daras <willdaras@gmail.com>
2024-04-02 18:52:36 +0100

    hover tint fix

--- UI/button.py
+++ UI/button.py
@@ -6,7 +6,7 @@ class Button(pygame.sprite.Sprite):
     def __init__(self, position: tuple[int, int], sprite: pygame.surface.Surface, on_clicked) -> None:
         super().__init__()
         self.original_image = sprite
-        self.image = sprite
+        self.image = sprite.copy()
         self.rect = self.image.get_rect(center=position)
         self.on_clicked = on_clicked
 
@@ -14,10 +14,10 @@ class Button(pygame.sprite.Sprite):
         if self.rect.collidepoint(input.get_pos()):
             self.on_hover()
         else:
-            self.image = self.original_image
+            self.image = self.original_image.copy()
 
     def on_hover(self):
-        self.image = self.original_image
+        self.image = self.original_image.copy()
         self.image.fill((100, 100, 100, 100), special_flags=pygame.BLEND_ADD)
         if input.l_pressed():
             self.on_clicked()
